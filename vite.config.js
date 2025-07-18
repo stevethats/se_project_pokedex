9b098244889d35b717a5c5bc5837664b652cbd98
stevethats <stevethats@yahoo.com>
2025-07-18 17:51:27 -0500

    readd define property

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/se_project_pokedex",
+  base: "/se_project_pokedex/",
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  define: {
+    global: "globalThis",
   },
   build: {
     commonjsOptions: {
